ba7d8364a37d49103111f9e514ca8aee1d86e335
chimerablack <christopherparke@hotmail.com>
2017-02-18 05:48:53 -0500

    game board and hands initially spaced out

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- custom CSS -->
+	  <link rel="stylesheet" href="normalize.css">
+	  <link rel="stylesheet" href="style.css">	  
+  </head>
+  <body>
+	  
+	  <div class="game-container">
+		  <div class="hand enemy-hand">
+		  
+		  </div>
+		  <div class="game-board">
+			  <div class="card-slot card-offset-left"></div><!-- 1 -->
+			  <div class="card-slot"></div><!-- 2 -->
+			  <div class="card-slot card-offset-right"></div><!-- 3 -->
+			  <div class="card-slot card-offset-left"></div><!-- 4 -->
+			  <div class="card-slot"></div><!-- 5 -->
+			  <div class="card-slot card-offset-right"></div><!-- 6 -->
+			  <div class="card-slot card-offset-left"></div><!-- 7 -->
+			  <div class="card-slot"></div><!-- 8 -->
+			  <div class="card-slot card-offset-right"></div><!-- 9 -->
+		  </div>
+		  <div class="hand player-hand">
+		  	
+		  </div>	  	
+	  </div>
+    
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+   
+  </body>
+	
+</html>
