a99d6d256840abc280eff7c1952d9b9b53dd9e94
ChimeraBlack1 <chimerablaque@hotmail.com>
2017-04-15 11:55:37 -0400

    magnified board slots now receive the hovered slot's background image.  Removes itself upon mouseLeave

--- hover.js
+++ hover.js
@@ -53,6 +53,69 @@ $(document).ready(function(){
 		$(".magSix").addClass("hidden");
     });
 	
+
+	
+	// ******************************************************************************
+	
+	// Board Slot One
+	$("#slot1").hover(function(){
+		var img = $("#slot1").css("background-image");
+		console.log(img);
+        $(".magSlotOne").css("background-image", img);
+        }, function(){
+        $(".magSlotOne").css("background-image", "");
+    });
+	
+	
+	// Board Slot Two
+	$("#slot2").hover(function(){
+		var img = $("#slot2").css("background-image");
+		console.log(img);
+        $(".magSlotTwo").css("background-image", img);
+        }, function(){
+        $(".magSlotTwo").css("background-image", "");
+    });
+	
+	
+	// Board Slot Three
+	$("#slot3").hover(function(){
+		var img = $("#slot3").css("background-image");
+		console.log(img);
+        $(".magSlotThree").css("background-image", img);
+        }, function(){
+        $(".magSlotThree").css("background-image", "");
+    });
+	
+	
+	// Board Slot Four
+	$("#slot4").hover(function(){
+		var img = $("#slot4").css("background-image");
+		console.log(img);
+        $(".magSlotFour").css("background-image", img);
+        }, function(){
+        $(".magSlotFour").css("background-image", "");
+    });
+	
+	
+	// Board Slot Five
+	$("#slot5").hover(function(){
+		var img = $("#slot5").css("background-image");
+		console.log(img);
+        $(".magSlotFive").css("background-image", img);
+        }, function(){
+        $(".magSlotFive").css("background-image", "");
+    });
+	
+	
+	// Board Slot Six
+	$("#slot6").hover(function(){
+		var img = $("#slot6").css("background-image");
+		console.log(img);
+        $(".magSlotSix").css("background-image", img);
+        }, function(){
+        $(".magSlotSix").css("background-image", "");
+    });
+	
 });
 
 
